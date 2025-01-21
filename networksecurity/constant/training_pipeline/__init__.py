0f14cf4e95215b5f99bcda47ea93a6b2d7c36dbe
debdoot9804 <debdutsen111@gmail.com>
2025-01-21 16:59:41 +0530

    Data ingestion component

---
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+#COMMON CONSTANT VARIABLE FOR TRAINING PIPELINE
+TARGET_COLUMN="Result"
+FILE_NAME: str ="PhisingData.csv"
+PIPELINE_NAME:str="Network Security"
+ARTIFACT_DIR: str="Artifacts"
+
+TRAIN_FILE_NAME: str ="Train.csv"
+TEST_FILE_NAME:str ="Test.csv"
+
+
+#DATA INGESTION RELATED CONSTANT
+DATA_INGESTION_COLLECTION_NAME:str ="NetworkData"
+DATA_INGESTION_DATABASE_NAME: str="DEBDOOT"
+DATA_INGESTION_DIR_NAME: str="data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str="feature_store"
+DATA_INGESTION_INGESTED_DIR: str="ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float=0.2
